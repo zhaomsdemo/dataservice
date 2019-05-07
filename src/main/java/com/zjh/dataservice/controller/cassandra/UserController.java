074b4430c4c4a4b6c288b85460bdff1aa5f99d65
zhaomsdemo <zhaomsdemo@gmail.com>
2019-05-06 21:03:23 -0700

    Add Solr

--- src/main/java/com/zjh/dataservice/controller/cassandra/UserController.java
+++ src/main/java/com/zjh/dataservice/controller/cassandra/UserController.java
@@ -2,6 +2,9 @@ package com.zjh.dataservice.controller.cassandra;
 
 import com.zjh.dataservice.entity.cassandra.User;
 import com.zjh.dataservice.service.cassandra.UserService;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrInputDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -17,9 +22,25 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private SolrClient solrClient;
+
     @PostMapping("/create")
-    public ResponseEntity<User> postUser(@RequestBody User user){
+    public ResponseEntity<User> postUser(@RequestBody User user) throws IOException, SolrServerException {
         User response = userService.saveUser(user);
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+        ResponseEntity<User> responseEntity = new ResponseEntity<User>(user, HttpStatus.OK);
+
+        saveToSolr(user);
+        return responseEntity;
+    }
+
+    private void saveToSolr(User user) throws IOException, SolrServerException {
+        SolrInputDocument document = new SolrInputDocument();
+        document.setField("userId", user.getUserId());
+        document.setField("userName", user.getUserName());
+        document.setField("age", user.getAge());
+
+        solrClient.add("demo", document);
+        solrClient.commit("demo");
     }
 }
